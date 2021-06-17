507e52bcfb3f630e270d07718a63a0e74b9964f0
Armando Ubario <70251597+mandouba@users.noreply.github.com>
2021-06-17 14:06:06 -0700

    Create Quiz.cpp

---
+++ Quiz.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+
+int main() {
+  int gryffindor = 0; 
+  int hufflepuff = 0;
+  int ravenclaw = 0;
+  int slytherin = 0; // each variable is declared
+
+  int answer1, answer2, answer3, answer4; // each amswer is gonna add a point to the variabes delcared and the one with most points is what the output is 
+  std::cout<<" The Sorting Hat Quiz! \n";
+
+  std::cout<<" Q1) When I'm dead, I want people to remember me as: \n\n";
+  std::cout << " 1) The Good \n";
+  std::cout << " 2) The Great \n";
+  std::cout << " 3) The Wise \n";
+  std::cout << " 4) The Bold \n";
+  std::cin >> answer1;
+
+  if (answer1 == 1){
+    hufflepuff ++;
+  }
+else if(answer1 == 2){
+  slytherin ++;
+
+}
+else if(answer1 == 3){
+  ravenclaw++;
+}
+else if(answer1 == 4){
+  gryffindor++;
+
+}
+else{
+  std::cout<<"Invalid input. \n";
+}
+
+std::cout<<" Q2) Dawn or Dusk? \n";
+std::cout<<" 1) Dawn \n";
+std::cout<<" 2) Dusk \n";
+std::cin>> answer2;
+
+if(answer2==1){
+  gryffindor++ && ravenclaw++;
+}
+
+else if(answer2==2){
+  hufflepuff++ && slytherin++;
+}
+else{ 
+  std::cout<<" Invalid input \n";
+}
+std::cout<<" Q3) Which kind of instrument most pleases your ear? \n";
+std::cout<<" 1) The Violin \n";
+std::cout<<" 2) The Trumpet \n";
+std::cout<<" 3) The Pian \n";
+std::cout<<" 4) The Drum \n";
+std::cin>> answer3;
+
+if(answer3==1) {
+  slytherin++;
+}
+else if( answer3==2) {
+hufflepuff++;
+}
+
+else if(answer3==3){
+    ravenclaw++;
+}
+else if(answer3==4){
+    gryffindor++;
+}
+else{
+    std::cout<<"Invalid input.\n";
+}
+std::cout<<" Q4) Which road tempts you the most? \n";
+std::cout<<" 1) The wide, sunny grassty lane \n";
+std::cout<<" 2) The narrow, dark, lanternlit alley \n";
+std::cout<<" 3) The twisting, leaf-strewn path through woods \n";
+std::cout<<" 4) The cobble street lined (ancient building) \n";
+std::cin>> answer4; 
+
+if(answer4== 1){
+  hufflepuff++;
+}
+else if(answer4==2){
+  slytherin++;
+}
+else if(answer4==3){
+  gryffindor++;
+}
+else if(answer4==4){
+  ravenclaw++;
+}
+else{
+  std::cout<<"Invalid input \n";
+
+}
+int max = 0;
+std::string house;
+if (gryffindor > max) {
+ 
+  max = gryffindor;
+  house = "Gryffindor";
+ 
+}
+ 
+if (hufflepuff > max) {
+ 
+  max = hufflepuff;
+  house = "Hufflepuff";
+ 
+}
+ 
+if (ravenclaw > max) {
+ 
+  max = ravenclaw;
+  house = "Ravenclaw";
+ 
+}
+ 
+if (slytherin > max) {
+ 
+  max = slytherin;
+  house = "Slytherin";
+ 
+}
+ 
+std::cout << house << "!\n";
+
